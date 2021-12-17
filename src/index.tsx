f6ddd64ce9da50746299f7d10715295d2ae4eba5
hulilei <huuuu1016@gmail.com>
2021-12-17 19:20:36 +0800

    reactnative api添加

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,42 @@ type FTMobileReactNativeType = {
   unbindRUMUserData(): Promise<void>;
 };
 
+type FTReactNativeLogType = {
+  logConfig(context?: object): Promise<void>;
+  logging(content:String,logStatus:number): Promise<void>;
+};
+
+type FTReactNativeRUMType = {
+  setConfig(rumAppId:String,context?:object): Promise<void>;
+  startAction(actionName:String,actionType:String): Promise<void>;
+  statView(viewName: String, viewReferer: String): Promise<void>;
+  stopView(): Promise<void>;
+  addError(stack: String, message: String): Promise<void>;
+  startResource(key: String): Promise<void>;
+  stopResource(key: String): Promise<void>;
+  addResource(
+    key: String,
+    url: String,
+    httpMethod: String,
+    context?: object,
+    ):Promise<void>;   
+}
+
+type FTReactNativeTraceType = {
+  setConfig(context?: object): Promise<void>;
+  getTraceHeader(key: String, url: String): Promise<object>; 
+  addTrace(
+    key: String,
+    httpMethod: String,
+    requestHeader:object,
+    statusCode?: number,
+    responseHeader?: object,
+    errorMessage?: String
+    ): Promise<void>;
+}
+
+
+
 class FTMobileReactNativeWrapper implements FTMobileReactNativeType {
   private sdk: FTMobileReactNativeType = NativeModules.FTMobileReactNative;
 
@@ -20,17 +56,85 @@ class FTMobileReactNativeWrapper implements FTMobileReactNativeType {
   unbindRUMUserData(): Promise<void> {
     return this.sdk.unbindRUMUserData();
   }
+
 }
 
-const FTMobileReactNative: FTMobileReactNativeType =
-  new FTMobileReactNativeWrapper();
-const FTReactNativeLog = NativeModules.FTReactNativeLog;
-const FTReactNativeRUM = NativeModules.FTReactNativeRUM;
-const FTReactNativeTrace = NativeModules.FTReactNativeTrace;
+class FTReactNativeLogWrapper implements FTReactNativeLogType {
+  private logger: FTReactNativeLogType = NativeModules.FTReactNativeLog;
 
+  logConfig(context:object = {}): Promise<void>{
+    return this.logger.logConfig(context);
+  }
+
+  logging(content:String,logStatus:number): Promise<void>{
+    return this.logger.logging(content,logStatus);
+  }
+
+}
+class FTReactNativeRUMWrapper implements FTReactNativeRUMType {
+  private rum: FTReactNativeRUMType = NativeModules.FTReactNativeRUM;
+
+  setConfig(rumAppId:String,context:object = {}): Promise<void>{
+    return this.rum.setConfig(rumAppId,context);
+  }
+  startAction(actionName:String,actionType:String): Promise<void>{
+    return this.rum.startAction(actionName,actionType);
+  }
+  statView(viewName: String, viewReferer: String): Promise<void>{
+    return this.rum.statView(viewName,viewReferer);
+  }
+  stopView(): Promise<void>{
+    return this.rum.stopView();
+  }
+  addError(stack: String, message: String): Promise<void>{
+    return this.rum.addError(stack,message);
+  }
+  startResource(key: String): Promise<void>{
+    return this.rum.startResource(key);
+  }
+  stopResource(key: String): Promise<void>{
+    return this.rum.stopResource(key);
+  }
+  addResource(
+    key: String,
+    url: String,
+    httpMethod: String,
+    context: object = {},
+    ):Promise<void>{
+   return this.rum.addResource(key,url,httpMethod,context);
+ }   
+}
+
+class FTReactNativeTraceWrapper implements FTReactNativeTraceType {
+ private trace: FTReactNativeTraceType = NativeModules.FTReactNativeTrace;
+
+ setConfig(context:object = {}): Promise<void>{
+   return this.trace.setConfig(context);
+ }
+
+ getTraceHeader(key:String,url:String): Promise<object>{
+  return this.trace.getTraceHeader(key,url);
+}
+
+ addTrace(
+  key:String,
+  httpMethod:String,
+  requestHeader:object,
+  statusCode:number = -1,
+  responseHeader:object = {},
+  errorMessage:String = ''){
+  return this.trace.addTrace(key,httpMethod,requestHeader,statusCode,responseHeader,errorMessage);
+}
+}
+
+const FTMobileReactNative: FTMobileReactNativeType = new FTMobileReactNativeWrapper();
+const FTReactNativeLog: FTReactNativeLogType = new FTReactNativeLogWrapper();
+const FTReactNativeRUM: FTReactNativeRUMType = new FTReactNativeRUMWrapper();
+const FTReactNativeTrace:FTReactNativeTraceType = new FTReactNativeTraceWrapper(); 
 export {
   FTMobileReactNative,
   FTReactNativeLog,
   FTReactNativeRUM,
   FTReactNativeTrace,
 };
+
