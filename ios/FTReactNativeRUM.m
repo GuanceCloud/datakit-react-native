f3914a83a366e7e9235d1a20ab7c07eb89abb7b6
hulilei <huuuu1016@gmail.com>
2024-08-14 22:12:04 +0800

    ios 过滤正则修改

--- ios/FTReactNativeRUM.m
+++ ios/FTReactNativeRUM.m
@@ -21,7 +21,7 @@ BOOL filterBlackResource(NSURL *url){
         NSString *pattern = @"^http://((10|172|192).[0-9]+.[0-9]+.[0-9]+|localhost|127.0.0.1):808[0-9]/logs$";
         NSRegularExpression * regularExpress = [NSRegularExpression regularExpressionWithPattern:pattern options:NSRegularExpressionCaseInsensitive error:&error];
         [internalDevResourceBlacklist addObject:regularExpress];
-        NSString *rn = @"^http://localhost:808[0-9]/(symbolicate$|assets)";
+        NSString *rn = @"^http://localhost:808[0-9]/(symbolicate$|status$|assets)";
         NSRegularExpression * rnRegularExpress = [NSRegularExpression regularExpressionWithPattern:rn options:NSRegularExpressionCaseInsensitive error:&error];
         [internalDevResourceBlacklist addObject:rnRegularExpress];
     });
