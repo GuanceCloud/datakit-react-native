00f230c973565152cfcfdbfb801ff6a1a5c93848
hulilei <huuuu1016@gmail.com>
2024-09-26 15:17:33 +0800

    补充注释

--- src/ft_session_replay.ts
+++ src/ft_session_replay.ts
@@ -1,16 +1,27 @@
 import { NativeModules } from 'react-native';
-
+/**
+ * 会话重播中内容屏蔽的隐私级别。
+ */
 export enum SessionReplayPrivacy {
     MASK ,
     ALLOW ,
     MASK_USER_INPUT 
 }
-
+/**
+ * 设置 Session Replay 配置.
+ * @param sampleRate 采样率
+ * @param privacy 会话重播中内容屏蔽的隐私级别
+ */
 export interface FTSessionReplayConfig{
     sampleRate?:number,
     privacy?:SessionReplayPrivacy
   }
 type FTReactNativeSessionReplayType = {
+  /**
+   * 设置 Session Replay 配置，开启会话重放
+   * @param config Session Replay 配置参数。
+   * @returns a Promise.
+   */
     sessionReplayConfig(config:FTSessionReplayConfig): Promise<void>;
 }
 
