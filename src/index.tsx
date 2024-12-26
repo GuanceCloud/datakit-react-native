4adfb959270dd601ecaa51ae97f75190a7a582f7
hulilei <huuuu1016@gmail.com>
2024-12-26 19:33:52 +0800

    export `FTRumErrorTracking` 与 `FTRumActionTracking`

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import {FTMobileConfig,FTMobileReactNative,EnvType} from './ft_mobile_agent'
 import {FTLogConfig,FTReactNativeLog,FTLogStatus,FTLogCacheDiscard} from './ft_logger'
 import {FTRUMConfig,FTRUMResource,FTReactNativeRUM,ErrorMonitorType,DeviceMetricsMonitorType,DetectFrequency,FTRUMResourceMetrics} from './ft_rum'
 import {FTTraceConfig,FTReactNativeTrace,TraceType} from './ft_tracing'
+import {FTRumActionTracking} from './rum/FTRumActionTracking'
+import {FTRumErrorTracking} from './rum/FTRumErrorTracking'
 
  
  export {
    FTMobileReactNative,FTMobileConfig,EnvType,
    FTLogConfig, FTReactNativeLog,FTLogStatus,FTLogCacheDiscard,
    FTRUMConfig,FTReactNativeRUM,FTRUMResource,ErrorMonitorType,DeviceMetricsMonitorType,DetectFrequency,FTRUMResourceMetrics,
-   FTTraceConfig,FTReactNativeTrace,TraceType
+   FTTraceConfig,FTReactNativeTrace,TraceType,
+   FTRumActionTracking,
+   FTRumErrorTracking
  };
 
