68d7c8a251aa2426398c82bbd92dd15af2f81ab6
hulilei <huuuu1016@gmail.com>
2025-07-15 14:00:18 +0800

    example-new-architecture:add `@mgcrea/metro-plugin-linked-packages` to devDependencies

--- example-new-architecture/metro.config.js
+++ example-new-architecture/metro.config.js
@@ -1,11 +1,18 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {
+  getLinkedPackagesConfig,
+} = require('@mgcrea/metro-plugin-linked-packages');
 
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(
+  getDefaultConfig(__dirname),
+  getLinkedPackagesConfig(__dirname),
+  config,
+);
