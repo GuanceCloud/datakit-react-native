83c464681216a3cf886f9cef21d51f999fe20df8
章博 <brandonfaustine@gmail.com>
2025-07-16 12:32:38 +0800

    Android: add sample code for install SDK from native code

--- example/android/app/src/main/java/com/cloudcare/ft/mobile/sdk/tracker/reactnative/example/MainApplication.kt
+++ example/android/app/src/main/java/com/cloudcare/ft/mobile/sdk/tracker/reactnative/example/MainApplication.kt
@@ -1,11 +1,9 @@
 package com.cloudcare.ft.mobile.sdk.tracker.reactnative.example
 
 import com.cloudcare.ft.mobile.sdk.tracker.reactnative.FTMobilePackage
+import com.cloudcare.ft.mobile.sdk.tracker.reactnative.sessionreplay.ReactNativeSessionReplayExtensionSupport
+import com.cloudcare.ft.mobile.sdk.tracker.reactnative.sessionreplay.utils.text.TextViewUtils
 import com.cloudcare.ft.mobile.sdk.tracker.reactnative.utils.ReactNativeUtils
-import com.ft.sdk.FTRUMConfig
-import com.ft.sdk.FTSDKConfig
-import com.ft.sdk.FTSdk
-
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -13,26 +11,34 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import com.ft.sdk.SessionReplay
+import com.ft.sdk.FTLoggerConfig
+import com.ft.sdk.FTRUMConfig
+import com.ft.sdk.FTSDKConfig
+import com.ft.sdk.FTSdk
+import com.ft.sdk.SessionReplayManager
 import com.ft.sdk.sessionreplay.FTSessionReplayConfig
 import com.ft.sdk.sessionreplay.SessionReplayPrivacy
+import com.ft.sdk.sessionreplay.material.MaterialExtensionSupport
 import com.reactnativenavigation.NavigationApplication
 import com.reactnativenavigation.react.NavigationReactNativeHost
+
+
 class MainApplication : NavigationApplication(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
-      object : NavigationReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-               add(FTMobilePackage())
-            }
-        override fun getJSMainModuleName(): String = "index"
+    object : NavigationReactNativeHost(this) {
+      override fun getPackages(): List<ReactPackage> =
+        PackageList(this).packages.apply {
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          add(FTMobilePackage())
+        }
 
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
+      override fun getJSMainModuleName(): String = "index"
+
+      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+      override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+      override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+    }
 
   override val reactHost: ReactHost
     get() = getDefaultReactHost(applicationContext, reactNativeHost)
@@ -44,25 +50,64 @@ class MainApplication : NavigationApplication(), ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
+    //guanceSDKInit()
   }
 
-  fun guanceSDKInit(){
-    val sdkConfig = FTSDKConfig.builder("datakitUrl")
+  private fun guanceSDKInit() {
+    // base SDK config
+    val sdkConfig = FTSDKConfig.builder(BuildConfig.DATAWAY_URL, BuildConfig.CLIENT_TOKEN)
     sdkConfig.isDebug = true
     FTSdk.install(sdkConfig)
-    val rumConfig = FTRUMConfig().setRumAppId("rumAppId")
+    // rum
+    val rumConfig = FTRUMConfig().setRumAppId(BuildConfig.ANDROID_APP_ID)
+    rumConfig.isEnableTrackAppANR = true
+    rumConfig.isEnableTrackAppCrash = true
+    rumConfig.isEnableTraceUserAction = true
     rumConfig.isEnableTraceUserResource = true
     if (BuildConfig.DEBUG) {
-      //Need to filter out symbolication requests and Expo log requests that only occur in the development environment.
+      //Need to filter out symbolication requests and Expo log requests that only occur
+      // in the development environment.
       rumConfig.setResourceUrlHandler { url ->
         return@setResourceUrlHandler ReactNativeUtils.isReactNativeDevUrl(url)
       }
     }
-    // ...
     FTSdk.initRUMWithConfig(rumConfig)
-    // ...
 
-    FTSdk.initSessionReplayConfig(FTSessionReplayConfig().setPrivacy(SessionReplayPrivacy.MASK_USER_INPUT))
+    //log
+    val logConfig = FTLoggerConfig().setEnableLinkRumData(true)
+    FTSdk.initLogWithConfig(logConfig)
+
+    //session replay
+    val sessionReplayConfig = FTSessionReplayConfig()
+    sessionReplayConfig
+      .setPrivacy(SessionReplayPrivacy.MASK_USER_INPUT)
+      .addExtensionSupport(MaterialExtensionSupport())//material
+    FTSdk.initSessionReplayConfig(sessionReplayConfig)
+
+  }
+
+  /**
+   *  ensure react native part loaded then call this
+   */
+  private fun reactNativeSessionReplayConfigInit() {
+    val sessionReplayConfig = FTSessionReplayConfig()
+    val reactContext = reactNativeHost.reactInstanceManager.currentReactContext
+    reactContext.let {
+      val textViewUtils = TextViewUtils.create(
+        it,
+        SessionReplayManager.get().internalLogger
+      )
+      sessionReplayConfig.addExtensionSupport(
+        ReactNativeSessionReplayExtensionSupport(
+          textViewUtils
+        )
+      )
+      sessionReplayConfig
+        .setPrivacy(SessionReplayPrivacy.MASK_USER_INPUT)
+        .addExtensionSupport(MaterialExtensionSupport())//material
+        .addExtensionSupport(ReactNativeSessionReplayExtensionSupport(textViewUtils))//react native part
+      FTSdk.initSessionReplayConfig(sessionReplayConfig)
+    }
   }
 
 }
