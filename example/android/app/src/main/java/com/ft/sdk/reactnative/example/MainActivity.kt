aae2e7277f54683deaa3115243fd8c941264df16
章博 <brandonfaustine@gmail.com>
2025-08-04 20:25:09 +0800

    Android: change the package name

---
+++ example/android/app/src/main/java/com/ft/sdk/reactnative/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ft.sdk.reactnative.example
+
+import com.reactnativenavigation.NavigationActivity
+
+class MainActivity : NavigationActivity() {
+}
--- example/android/app/src/main/java/com/ft/sdk/reactnative/example/MainApplication.kt
+++ example/android/app/src/main/java/com/ft/sdk/reactnative/example/MainApplication.kt
@@ -1,20 +1,20 @@
-package com.cloudcare.ft.mobile.sdk.tracker.reactnative.example
-
-import com.ft.sdk.reactnative.FTMobilePackage
-import com.ft.sdk.reactnative.utils.ReactNativeUtils
-import com.ft.sdk.FTRUMConfig
-import com.ft.sdk.FTSDKConfig
-import com.ft.sdk.FTSdk
+package com.ft.sdk.reactnative.example
 
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultReactHost
+import com.ft.sdk.FTRUMConfig
+import com.ft.sdk.FTSDKConfig
+import com.ft.sdk.FTSdk
+import com.ft.sdk.reactnative.FTMobilePackage
+import com.ft.sdk.reactnative.utils.ReactNativeUtils
 import com.reactnativenavigation.NavigationApplication
 import com.reactnativenavigation.react.NavigationReactNativeHost
+
 class MainApplication : NavigationApplication(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
@@ -32,14 +32,14 @@ class MainApplication : NavigationApplication(), ReactApplication {
       }
 
   override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, reactNativeHost)
+    get() = DefaultReactHost.getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
 //    SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+        DefaultNewArchitectureEntryPoint.load()
 //      // Hybrid development SDK initialization example
 //      FTMobileConfig config = new FTMobileConfig("datakitUrl");
 //      config.setEnableSDKDebugLog(true);
@@ -58,7 +58,7 @@ class MainApplication : NavigationApplication(), ReactApplication {
   fun guanceSDKInit(){
     val sdkConfig = FTSDKConfig.builder("datakitUrl")
     sdkConfig.isDebug = true
-    FTSdk.install(sdkConfig)
+      FTSdk.install(sdkConfig)
     val rumConfig = FTRUMConfig().setRumAppId("rumAppId")
     rumConfig.isEnableTraceUserResource = true
     if (BuildConfig.DEBUG) {
@@ -68,7 +68,7 @@ class MainApplication : NavigationApplication(), ReactApplication {
       }
     }
     // ...
-    FTSdk.initRUMWithConfig(rumConfig)
+      FTSdk.initRUMWithConfig(rumConfig)
     // ...
   }
 
