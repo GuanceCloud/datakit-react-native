33e35b0fd4d25de59d83de7e35d173e67214c62b
hulilei <huuuu1016@gmail.com>
2026-09-04 15:19:44 +0800

    fix(ios): filter React Native inspector resources

--- example-new-architecture/ios/FTSdkReactNativeExampleTests/FTSdkReactNativeExampleTests.m
+++ example-new-architecture/ios/FTSdkReactNativeExampleTests/FTSdkReactNativeExampleTests.m
@@ -3,6 +3,7 @@
 
 #import <React/RCTLog.h>
 #import <React/RCTRootView.h>
+#import <FTMobileReactNativeSDK/FTReactNativeUtils.h>
 
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
@@ -12,6 +13,34 @@
 @end
 
 @implementation FTSdkReactNativeExampleTests
+
+- (void)testFiltersReactNativeDevelopmentResources
+{
+  NSArray<NSString *> *urls = @[
+    @"http://localhost:8081/inspector/device?name=iPhone",
+    @"http://192.168.1.20:8081/inspector/device?name=iPhone",
+    @"ws://10.0.2.2:8081/message?device=emulator",
+    @"http://127.0.0.1:8088/debugger-proxy?role=client",
+    @"http://[::1]:8081/logs",
+  ];
+
+  for (NSString *urlString in urls) {
+    XCTAssertTrue([FTReactNativeUtils filterBlackResource:[NSURL URLWithString:urlString]], @"Expected %@ to be filtered", urlString);
+  }
+}
+
+- (void)testDoesNotFilterApplicationResources
+{
+  NSArray<NSString *> *urls = @[
+    @"https://api.example.com/inspector/device",
+    @"http://192.168.1.20:9529/inspector/device",
+    @"http://192.168.1.20:8081/business",
+  ];
+
+  for (NSString *urlString in urls) {
+    XCTAssertFalse([FTReactNativeUtils filterBlackResource:[NSURL URLWithString:urlString]], @"Expected %@ not to be filtered", urlString);
+  }
+}
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
--- packages/react-native-mobile/ios/FTReactNativeUtils.m
+++ packages/react-native-mobile/ios/FTReactNativeUtils.m
@@ -9,24 +9,19 @@
 
 @implementation FTReactNativeUtils
 + (BOOL)filterBlackResource:(NSURL *)url{
-    static NSMutableArray *internalDevResourceBlacklist;
+    static NSRegularExpression *internalDevResourceRegex;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        internalDevResourceBlacklist = [NSMutableArray new];
-        NSError *error = nil;
-        NSString *pattern = @"^http://((10|172|192).[0-9]+.[0-9]+.[0-9]+|localhost|127.0.0.1):808[0-9]/logs$";
-        NSRegularExpression * regularExpress = [NSRegularExpression regularExpressionWithPattern:pattern options:NSRegularExpressionCaseInsensitive error:&error];
-        [internalDevResourceBlacklist addObject:regularExpress];
-        NSString *rn = @"^http://localhost:808[0-9]/(hot|symbolicate|message|inspector|status|assets).*$";
-        NSRegularExpression * rnRegularExpress = [NSRegularExpression regularExpressionWithPattern:rn options:NSRegularExpressionCaseInsensitive error:&error];
-        [internalDevResourceBlacklist addObject:rnRegularExpress];
+        NSString *pattern = @"^(?:https?|wss?)://(?:(?:10|172|192)(?:\\.[0-9]+){3}|localhost|127\\.0\\.0\\.1|\\[::1\\]):808[0-9]/(?:hot|symbolicate|message|inspector|status|assets|logs|debugger-proxy)(?:[/?#].*)?$";
+        internalDevResourceRegex = [NSRegularExpression regularExpressionWithPattern:pattern
+                                                                             options:NSRegularExpressionCaseInsensitive
+                                                                               error:NULL];
     });
-    for (NSRegularExpression *regex in internalDevResourceBlacklist) {
-        NSTextCheckingResult *firstMatch =[regex firstMatchInString:url.absoluteString options:0 range:NSMakeRange(0, [url.absoluteString length])];
-        if (firstMatch) {
-            return YES;
-        }
+    NSString *absoluteString = url.absoluteString;
+    if (absoluteString.length == 0) {
+        return NO;
     }
-    return NO;
+    NSRange fullRange = NSMakeRange(0, absoluteString.length);
+    return [internalDevResourceRegex firstMatchInString:absoluteString options:0 range:fullRange] != nil;
 }
 @end
